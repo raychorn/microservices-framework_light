07435111d213a5e6f11bba078c63b78de69b8a21
Ray C Horn <raychorn@gmail.com>
2021-02-17 20:10:25 -0700

    updates.

--- httpd.py
+++ httpd.py
@@ -51,6 +51,7 @@ service_runner = ServiceRunner(__env__.get('plugins'), logger=None, debug=is_deb
 def catch_all(path):
     the_response = {"path": path}
     the_path = path.split('/')
+    __fp_plugins__ = [__env__.get('plugins')]
     print('request.method -> {}'.format(request.method))
     if (request.method in ['POST', 'PUT', 'DELETE']):
         try:
@@ -64,33 +65,56 @@ def catch_all(path):
         try:
             the_response = {}
             uuid = the_path[0]
-            func = the_path[1]
-            if (func == '__directory__'):
-                if (uuid == __env__.get('__uuid__')):
-                    the_response['fpath'] = os.path.dirname(__file__)
-                    __fp_plugins__ = [__env__.get('plugins')]
-                    the_response['__fp_plugins__'] = {}
-                    for fp_plugins in __fp_plugins__:
-                        the_response['__fp_plugins__'][fp_plugins] = {}
-                        the_response['__fp_plugins__'][fp_plugins]['has_plugins'] = has_plugins = os.path.exists(fp_plugins) and os.path.isdir(fp_plugins)
-                        if (not has_plugins):
-                            os.mkdir(fp_plugins)
-                            the_response['__fp_plugins__'][fp_plugins]['has_plugins'] = has_plugins = os.path.exists(fp_plugins) and os.path.isdir(fp_plugins)
-                        fp_plugins_initpy = os.sep.join([fp_plugins, '__init__.py'])
-                        if (not (os.path.exists(fp_plugins_initpy) and (os.path.isfile(fp_plugins_initpy)))):
-                            with open(fp_plugins_initpy, 'w') as fOut:
-                                fOut.write('{}\n'.format('#'*40))
-                                fOut.write('# (c). Copyright, Vyper Logix Corp, All Rights Reserved.\n')
-                                fOut.write('{}\n'.format('#'*40))
-                                fOut.write('\n\n')
-                                fOut.flush()
-                        if (is_debugging or request.args.get('DEBUG', False)):
-                            the_response['__fp_plugins__'][fp_plugins]['query_params'] = request.args
-                            the_response['__fp_plugins__'][fp_plugins]['modules'] = service_runner.modules.get(fp_plugins)
-                            the_response['__fp_plugins__'][fp_plugins]['endpoints'] = expose.get_endpoints(for_root=fp_plugins)
-                            the_response['__fp_plugins__'][fp_plugins]['imports'] = service_runner.imports.get(fp_plugins)
-                            the_response['__fp_plugins__'][fp_plugins]['aliases'] = service_runner.aliases.get(fp_plugins)
-                            the_response['__fp_plugins__'][fp_plugins]['status'] = 'OK'
+            module_name = the_path[1]
+            if (uuid == __env__.get('__uuid__')):
+                if (module_name == '__directory__'):
+                    if (uuid == __env__.get('__uuid__')):
+                        the_response['fpath'] = os.path.dirname(__file__)
+                        the_response['__plugins__'] = {}
+                        for fp_plugins in __fp_plugins__:
+                            the_response['__plugins__'][fp_plugins] = {}
+                            the_response['__plugins__'][fp_plugins]['has_plugins'] = has_plugins = os.path.exists(fp_plugins) and os.path.isdir(fp_plugins)
+                            if (not has_plugins):
+                                os.mkdir(fp_plugins)
+                                the_response['__plugins__'][fp_plugins]['has_plugins'] = has_plugins = os.path.exists(fp_plugins) and os.path.isdir(fp_plugins)
+                            fp_plugins_initpy = os.sep.join([fp_plugins, '__init__.py'])
+                            if (not (os.path.exists(fp_plugins_initpy) and (os.path.isfile(fp_plugins_initpy)))):
+                                with open(fp_plugins_initpy, 'w') as fOut:
+                                    fOut.write('{}\n'.format('#'*40))
+                                    fOut.write('# (c). Copyright, Vyper Logix Corp, All Rights Reserved.\n')
+                                    fOut.write('{}\n'.format('#'*40))
+                                    fOut.write('\n\n')
+                                    fOut.flush()
+                            if (is_debugging or request.args.get('DEBUG', False)):
+                                the_response['__plugins__'][fp_plugins]['query_params'] = request.args
+                                the_response['__plugins__'][fp_plugins]['modules'] = service_runner.modules.get(fp_plugins)
+                                the_response['__plugins__'][fp_plugins]['endpoints'] = expose.get_endpoints(for_root=fp_plugins)
+                                the_response['__plugins__'][fp_plugins]['imports'] = service_runner.imports.get(fp_plugins)
+                                the_response['__plugins__'][fp_plugins]['aliases'] = service_runner.aliases.get(fp_plugins)
+                                the_response['__plugins__'][fp_plugins]['status'] = 'OK'
+                else:
+                    d = None
+                    func_name = the_path[2]
+                    the_args = the_path[3:]
+                    for root in __fp_plugins__:
+                        endpoints = expose.get_endpoints(for_root=root).get(root, {})
+                        d = endpoints.get(request.method, {})
+                        if (module_name in list(service_runner.aliases.get(root, {}).keys())):
+                            module_name = service_runner.aliases.get(root, {}).get(module_name)
+                        if (module_name in list(service_runner.modules.get(root, {}).keys())):
+                            fq_func_name = '{}.{}'.format(module_name, func_name)
+                            d = d.get(fq_func_name, None)
+                            if (d is not None) and (len(d) > 0):
+                                break
+                    if (d is not None):
+                        data = {}
+                        for k,v in request.args.items():
+                            data[k] = v
+                        for i in range(1, len(the_args)+1):
+                            data['p{}'.format(i)] = the_args[i-1]
+                        the_response['response'] = service_runner.exec(module_name, func_name, **data)
+            else:
+                return json.dumps({'success':False}), 404, {'ContentType':'application/json'}
         except Exception as ex:
             print(_utils.formattedException(details=ex))
     return Response(json.dumps(dictutils.json_cleaner(the_response)), mimetype='application/json')
