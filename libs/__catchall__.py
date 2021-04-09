105a69668b1f40ce4ec25ef3601ce36b79e2f43d
Ray C Horn <raychorn@gmail.com>
2021-04-09 08:05:24 -0600

    updates. fixed an issue with how responses were being handled.

--- libs/__catchall__.py
+++ libs/__catchall__.py
@@ -5,6 +5,41 @@ from vyperlogix.decorators import expose
 
 normalize_query_params = lambda r : dict([tuple([k,v]) for k,v in r.items()])
 
+
+def __response_handler__(the_response, request=None, response_handler=None, dictutils=None, is_serverMode_django=None, mimetype='application/json', logger=None):
+    __response__ = None
+    if (callable(response_handler)):
+        try:
+            __response__ = response_handler(json.dumps(dictutils.json_cleaner(the_response)) if ((not callable(is_serverMode_django)) or (not is_serverMode_django())) else the_response, mimetype='application/json')
+        except Exception as ex:
+            logger.exception('EXCEPTION -> {}'.format(request.method), ex)
+    return __response__
+    
+
+class CatchAllResponse(object):
+    
+    def __init__(self):
+        ...
+
+    def __call__(self, f):
+        try:
+            def wrapped_f(*args, **kwargs):
+                the_response = f(*args, **kwargs)
+                __kwargs = {}
+                __kwargs['request'] = kwargs.get('request')
+                __kwargs['response_handler'] = kwargs.get('response_handler')
+                __kwargs['dictutils'] = kwargs.get('dictutils')
+                __kwargs['is_serverMode_django'] = kwargs.get('is_serverMode_django')
+                __kwargs['mimetype'] = kwargs.get('mimetype')
+                __kwargs['logger'] = kwargs.get('logger')
+                return __response_handler__(the_response, **__kwargs)
+            return wrapped_f
+        except Exception as ex:
+            if (self.logger):
+                self.logger.exception('EXCEPTION -> {}'.format(self.request.method), ex)
+
+
+@CatchAllResponse()
 def __catch_all__(path, request=None, response_handler=None, __json=None, logger=None, service_runner=None, is_serverMode_flask=None, is_serverMode_django=None, __env__=None, is_debugging=False, dictutils=None):
     def request_query_params(request, is_serverMode_flask=None, is_serverMode_django=None):
         return normalize_query_params(request.args if (is_serverMode_flask()) else request.query_params if ((not callable(is_serverMode_django)) or (not is_serverMode_django())) else request.GET)
@@ -25,12 +60,12 @@ def __catch_all__(path, request=None, response_handler=None, __json=None, logger
                 if (d is not None):
                     the_response['response'] = d
                 else:
-                    return json.dumps({'success':False}), 404, {'ContentType':'application/json'}
+                    return {'success':False}, 404, {'ContentType':'application/json'}
             else:
-                return json.dumps({'success':False}), 404, {'ContentType':'application/json'}
+                return {'success':False}, 404, {'ContentType':'application/json'}
         except Exception as ex:
             logger.exception('EXCEPTION -> {}'.format(request.method), ex)
-            return json.dumps({'success':False, 'reason': str(ex)}), 404, {'ContentType':'application/json'}
+            return {'success':False, 'reason': str(ex)}, 404, {'ContentType':'application/json'}
             
     elif (request.method in ['GET']):
         try:
@@ -69,17 +104,12 @@ def __catch_all__(path, request=None, response_handler=None, __json=None, logger
                     if (d is not None):
                         the_response['response'] = d
                     else:
-                        return json.dumps({'success':False}), 404, {'ContentType':'application/json'}
+                        return {'success':False}, 404, {'ContentType':'application/json'}
             else:
-                return json.dumps({'success':False}), 404, {'ContentType':'application/json'}
+                return {'success':False}, 404, {'ContentType':'application/json'}
         except Exception as ex:
             logger.exception('EXCEPTION -> {}'.format(request.method), ex)
-            return json.dumps({'success':False, 'reason': str(ex)}), 404, {'ContentType':'application/json'}
-    __response__ = None
-    if (callable(response_handler)):
-        try:
-            __response__ = response_handler(json.dumps(dictutils.json_cleaner(the_response)) if ((not callable(is_serverMode_django)) or (not is_serverMode_django())) else the_response, mimetype='application/json')
-        except Exception as ex:
-            logger.exception('EXCEPTION -> {}'.format(request.method), ex)
-    return __response__
+            return {'success':False, 'reason': str(ex)}, 404, {'ContentType':'application/json'}
+    #__response__ = __response_handler__(the_response, request=request, response_handler=response_handler, dictutils=dictutils, is_serverMode_django=is_serverMode_django, mimetype='application/json', logger=logger)
+    return the_response
 
