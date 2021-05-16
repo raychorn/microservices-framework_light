a4c362e7416428cca9e11c7e0b9f4a9dff4bbed7
Ray C Horn <raychorn@gmail.com>
2021-05-16 13:58:04 -0600

    updates.

---
+++ aws/auto-ecr-rest-api/sample-post-upload.py
@@ -0,0 +1,66 @@
+import requests
+import json
+
+import time
+from concurrent import futures
+
+def conversion_task(item=None):
+    return [item[0], item[-1].hex()]
+
+
+def read_binary_file(fpath, chunksize=2048, max_workers=100):
+    startTime = time.time()
+    datum = []
+    wait_for = []
+    if (max_workers > 0):
+        executor = futures.ThreadPoolExecutor(max_workers=max_workers)
+    with open(fpath, 'rb') as fIn:
+        num = 0
+        while (data := fIn.read(chunksize)):
+            if (max_workers > 0):
+                wait_for.append(executor.submit(conversion_task, [num, data]))
+            else:
+                datum.append([num, data.hex()])
+            num += 1
+            
+    if (max_workers == 0):
+        print('{} chunks.'.format(len(datum)))
+    else:
+        for f in futures.as_completed(wait_for):
+            datum.append(f.result())
+        print('{} chunks.'.format(len(datum)))
+        
+    if (max_workers == 0):
+        assert num == len(datum), 'Something went wrong with the threaded thing because there should be {} chunks but there were {} chunks seen.'.format(num, len(datum))
+    else:
+        assert num == len(datum), 'Something went wrong with the threaded thing because there should be {} chunks but there were {} chunks seen.'.format(num, len(datum))
+    executionTime = (time.time() - startTime)
+    print('Done. {:.2f}'.format(executionTime))
+    return executionTime
+    
+dt1 = read_binary_file('/home/raychorn/projects/python-projects/securex.ai/data/docker-images/raychorn-appgenerator-dev-latest.tar.gz', max_workers=0)
+dt2 = read_binary_file('/home/raychorn/projects/python-projects/securex.ai/data/docker-images/raychorn-appgenerator-dev-latest.tar.gz', max_workers=500)
+
+if (dt1 < dt2):
+    print('#1 {} ran faster than #2 {}'.format(dt1, dt2))
+else:
+    print('#1 {} ran slower than #2 {}'.format(dt1, dt2))
+
+headers={'Content-Type':'application/gzip'}
+f = open('/home/raychorn/projects/python-projects/securex.ai/data/docker-images/raychorn-appgenerator-dev-latest.tar.gz', 'rb')
+
+files = {"file": f}
+
+URL = 'http://172.19.152.76:9999/4a1bf01e-0693-48c5-a52b-fc275205c1d8/auto-ecr-rest-api-v1/submit-job-details/'
+
+resp = requests.post(URL, files=files, headers=headers )
+
+print(resp.text)
+
+print("status code " + str(resp.status_code))
+
+if resp.status_code == 200:
+    print ("Success")
+    print(resp.json())
+else:
+    print("Failure")
