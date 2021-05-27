38969bc0525d6a14ee66c20fef208e0cc24ec514
Ray C Horn <raychorn@gmail.com>
2021-05-27 16:57:02 -0600

    updates.

--- aws/automated-ecr/libs/__utils__.py
+++ aws/automated-ecr/libs/__utils__.py
@@ -1,11 +1,12 @@
 import os
 import re
+import types
 
 import boto3
 
 from pathlib import Path
 
-is_really_something = lambda s,t:(s is not None) and (((not callable(t)) and isinstance(s, t)) or ((callable(t) and t(s))))
+is_really_something = lambda s,t:(s is not None) and ( (callable(t) and (not isinstance(t, types.FunctionType)) and isinstance(s, t)) or (callable(t) and (isinstance(t, types.FunctionType)) and t(s)) )
 is_really_something_with_stuff = lambda s,t:is_really_something(s,t) and (len(s) > 0)
 something_greater_than_zero = lambda s:(s > 0)
 default_timestamp = lambda t:t.isoformat().replace(':', '').replace('-','').split('.')[0]
